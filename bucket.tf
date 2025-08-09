28aa8044e0536eb980b0aa1089bf04695dfb7ad4
smartsolutionslab360 <smartsolutionslab360@gmail.com>
2025-08-09 16:33:15 +0200

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -8,11 +8,14 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
   endpoints {
-    s3 = "http://host.docker.internal:4566"
+    s3 = "http://localstack:4566"
   }
 }
 
