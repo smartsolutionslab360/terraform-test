b05585b8b9def85ded7fb3ce3dc87b9e3d947c5e
smartsolutionslab360 <smartsolutionslab360@gmail.com>
2025-08-09 15:20:20 +0000

    ch

--- lambda_xmp/lambda-xmp.tf
+++ lambda_xmp/lambda-xmp.tf
@@ -42,6 +42,52 @@ resource "aws_iam_role_policy" "lambda_xmp_s3_policy" {
   })
 }
 
+resource "aws_api_gateway_rest_api" "api_xmp" {
+  name        = "xmp-api"
+  description = "API Gateway para Lambda XMP"
+}
+
+resource "aws_api_gateway_resource" "proxy_xmp" {
+  rest_api_id = aws_api_gateway_rest_api.api_xmp.id
+  parent_id   = aws_api_gateway_rest_api.api_xmp.root_resource_id
+  path_part   = "process-xmp"
+}
+
+resource "aws_api_gateway_method" "get_xmp" {
+  rest_api_id   = aws_api_gateway_rest_api.api_xmp.id
+  resource_id   = aws_api_gateway_resource.proxy_xmp.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_xmp" {
+  rest_api_id             = aws_api_gateway_rest_api.api_xmp.id
+  resource_id             = aws_api_gateway_resource.proxy_xmp.id
+  http_method             = aws_api_gateway_method.get_xmp.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.xmp_processor.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_xmp" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.xmp_processor.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_xmp.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "deploy_xmp" {
+  depends_on  = [aws_api_gateway_integration.lambda_xmp]
+  rest_api_id = aws_api_gateway_rest_api.api_xmp.id
+}
+
+resource "aws_api_gateway_stage" "stage_xmp" {
+  rest_api_id   = aws_api_gateway_rest_api.api_xmp.id
+  deployment_id = aws_api_gateway_deployment.deploy_xmp.id
+  stage_name    = "dev"
+}
+
 resource "aws_lambda_layer_version" "ffmpeg_layer" {
   filename         = "${path.module}/ffmpeg-layer.zip"
   layer_name       = "ffmpeg-layer"
