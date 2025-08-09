49bbe42446f1a1e69ee6e0944f2af9eba70e19cf
smartsolutionslab360 <smartsolutionslab360@gmail.com>
2025-08-09 16:34:43 +0200

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region                      = "us-east-1"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   s3_force_path_style         = true
   endpoints {
     s3 = "http://localstack:4566"
