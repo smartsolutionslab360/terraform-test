2136226913d3078f1a34e2fd052a583af4c1beb6
smartsolutionslab360 <smartsolutionslab360@gmail.com>
2025-08-09 14:47:51 +0000

    ch

--- lambda/lambda-api.tf
+++ lambda/lambda-api.tf
@@ -53,6 +53,12 @@ resource "aws_api_gateway_method" "get" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deploy.id
+  stage_name    = "dev"
+}
+
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.proxy.id
@@ -73,5 +79,4 @@ resource "aws_lambda_permission" "apigw" {
 resource "aws_api_gateway_deployment" "deploy" {
   depends_on  = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "dev"
 }
